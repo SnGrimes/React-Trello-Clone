6fededc6b81db08e33c77eed33ffe3970374fdd8
Shantia Grimes <shantiagrimes@gmail.com>
2018-05-14 10:54:40 -0500

    added more list compenent unit tests

--- react-trello-clone/src/__tests__/list.test.js
+++ react-trello-clone/src/__tests__/list.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow} from 'enzyme'
 import List from '../components/list';
 
 describe('List', () => {
-  it('renders correctly', () => {
-    expect(List).toMatchSnapshot();
+  const listTitle = 'Test1';
+  const props = {key: listTitle, listTitle: listTitle};
+  const list = shallow(<List {...props}/>);
+  const mockAddCard = jest.fn();
+    jest.mock('../components/list', () => {
+    return jest.fn().mockImplementation(() => {
+    return {addCard : mockAddCard};
   });
+});
+  
+  it('renders correctly', () => {
+    expect(list).toMatchSnapshot();
+  });
+  it('receives the props', () => {
+    expect(list.instance().props.listTitle).toEqual(listTitle);
+  });
+  it('initializes `state` with an empty card array', () => {
+    expect(list.state().cards).toEqual([]);
+  });
+  describe('when clicking the `newCard`', () => {
+    beforeEach(() => {
+      list.find('.list__new-card').simulate('click');      
+    });
+    
+    it('calls the `list__new-card` button', () => {
+      expect(mockAddCard).toHaveBeenCalled;
+    });
+    
+  });
+
 });
--- react-trello-clone/src/components/list.js
+++ react-trello-clone/src/components/list.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+  constructor(props){
+    super(props);
+    this.addCard = this.addCard.bind(this);
+    this.state = {
+      cards: []
+    }
+  }
+
+  addCard() {
+    console.log('Add card was called');
+  }
   render() {
     return (
       <div className="cell small-3 small-offset-0 list">
         <h5>{this.props.listTitle}</h5>
-        <button className="button small">Add Card</button>
+        <button className="list__new-card button small" onClick={this.addCard}>Add Card</button>
       </div>
     );
   }
